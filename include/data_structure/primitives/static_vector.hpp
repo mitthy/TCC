639efcc6c1a880a4c533739d24e4119c8baefedf
Lucas Finger Roman <lfrfinger@gmail.com>
2019-08-26 12:10:22 -0300

    Adding memory management operations for static vector class

--- include/data_structure/primitives/static_vector.hpp
+++ include/data_structure/primitives/static_vector.hpp
@@ -14,46 +14,55 @@ namespace tcc {
 
     namespace __detail__ {
 
-      template<typename T>
-      void __init_mem_with_list__(std::initializer_list<T> args, T* data) {
-        std::uninitialized_copy( std::begin( args ), std::end( args ), data );
-      }
-
       template< typename Sub, typename T, size_t Size >
-      struct __safe_insert_op__ {
+      struct __safe_vec__ {
 
         template<typename... Args>
-        int push_back( Args&&... args ) {
+        int
+        push_back( Args&&... args ) {
           static_assert( sizeof...( args ) <= Size, "Can't push back more objects than maximum size!" );
-          static_assert( (std::is_same_v<T, Args> && ...), "Args and T should be the same type." );
+          static_assert( ( std::is_same_v<T, std::decay_t<Args>> && ... ), "Args and T should be the same type." );
           auto& self = *static_cast<Sub*>( this );
           if( self.size() + sizeof...( args ) > Size ) {
             return -1;
           }
-          __init_mem_with_list__( { std::forward<Args>(args)... }, self.m_end );
-          self.m_end += sizeof...( args );
+          ( self.construct( std::forward<Args>( args ) ), ... );
+          return 0;
+        }
+
+        int
+        pop() {
+          auto& self = *static_cast<Sub*>( this );
+          if( self.is_empty() ) {
+            return -1;
+          }
+          --self.m_end;
           return 0;
         }
 
       };
 
       template< typename Sub, typename T, size_t Size >
-      struct __unsafe_insert_op__ {
+      struct __unsafe_vec__ {
 
         template<typename... Args>
-        void push_back( Args&&... args ) {
+        void
+        push_back( Args&&... args ) {
           static_assert( sizeof...( args ) <= Size, "Can't push back more objects than maximum size!" );
-          static_assert( (std::is_same_v<T, Args> && ...), "Args and T should be the same type." );
+          static_assert( ( std::is_same_v<T, std::decay_t<Args>> && ... ), "Args and T should be the same type." );
           auto& self = *static_cast<Sub*>( this );
-          __init_mem_with_list__( { std::forward<Args>(args)... }, self.m_end );
-          self.m_end += sizeof...( args );
+          ( self.construct( std::forward<Args>( args ) ), ... );
         }
-        
+
+        void
+        pop() {
+          auto& self = *static_cast<Sub*>( this );
+          --self.m_end;
+        }
+
       };
 
     }
-
-
 
     //We can't use std array cause type may not be default initialised...
     template< typename T, size_t Size, template< typename, typename, size_t > class OpPolicy >
@@ -69,33 +78,63 @@ private:
 
       T* m_end;
 
+      template< typename... Args >
+      void
+      construct( Args&&... args ) {
+        new( m_end++ ) T( std::forward<Args>( args )... );
+      }
+
 public:
 
-      static_vector(): m_data(), m_end(reinterpret_cast<T*>(&m_data[0])) {}
+      static_vector(): m_data(), m_end( reinterpret_cast<T*>( &m_data[ 0 ] ) ) {}
 
-      constexpr bool is_empty() const {
+      static_vector( const static_vector& rhs ): m_end( std::uninitialized_copy( reinterpret_cast<const T*>( &rhs.m_data[ 0 ] ),
+                                                        static_cast<const T*>( rhs.m_end ), reinterpret_cast<T*>( &m_data[ 0 ] ) ) ) {
+      }
+      
+      static_vector&
+      operator=( const static_vector& rhs ) {
+        m_end = std::copy( reinterpret_cast<const T*>( &rhs.m_data[ 0 ] ), static_cast<const T*>( rhs.m_end ), reinterpret_cast<T*>( &m_data[ 0 ] ) );
+        return *this;
+      }
+
+      constexpr bool
+      is_empty() const {
         return reinterpret_cast<const T*>( &m_data[ 0 ] ) == m_end;
       }
 
-      constexpr size_t size() const {
+      constexpr size_t
+      size() const {
         return m_end - reinterpret_cast<const T*>( &m_data[ 0 ] );
       }
 
-      T& operator[]( size_t pos ) {
+      T&
+      operator[]( size_t pos ) {
         return *reinterpret_cast<T*>( &m_data[ pos ] );
       }
 
-      const T& operator[]( size_t pos ) const {
+      const T&
+      operator[]( size_t pos ) const {
         return *reinterpret_cast<T*>( &m_data[ pos ] );
+      }
+
+      ~static_vector() {
+        if constexpr(!std::is_trivially_destructible_v<T>) {
+          auto begin = reinterpret_cast<T*>( &m_data[ 0 ] );
+          while( begin != m_end ) {
+            begin->~T();
+            ++begin;
+          }
+        }
       }
 
     };
 
     template< typename T, size_t Size>
-    using safe_static_vector = static_vector<T, Size, __detail__::__safe_insert_op__>;
+    using safe_static_vector = static_vector<T, Size, __detail__::__safe_vec__>;
 
     template< typename T, size_t Size>
-    using unsafe_static_vector = static_vector<T, Size, __detail__::__unsafe_insert_op__>;
+    using unsafe_static_vector = static_vector<T, Size, __detail__::__unsafe_vec__>;
 
   }
 
--- tests/data_structure/primitives/test_static_vector.cpp
+++ tests/data_structure/primitives/test_static_vector.cpp
@@ -13,5 +13,178 @@ TEST( TestStaticVector, TestSafeInsert ) {
   EXPECT_FALSE( one_element_vec.is_empty() );
   EXPECT_EQ( one_element_vec[0], 3 );
   EXPECT_EQ( one_element_vec.size(), 1 );
-  EXPECT_EQ( one_element_vec.push_back( 5 ), -1 ); 
+  EXPECT_EQ( one_element_vec.push_back( 5 ), -1 );
+}
+
+TEST( TestStaticVector, TestSafePopEmptyVector ) {
+  tcc::data_structure::safe_static_vector<int, 32> empty{};
+  EXPECT_EQ( empty.pop(), -1 );
+  EXPECT_EQ( empty.push_back( 666 ), 0 );
+  EXPECT_EQ( empty[0], 666 ); //Ensure no side effects.
+  EXPECT_EQ( empty.pop(), 0 );
+}
+
+TEST( TestStaticVector, TestSafePopPopulatedVector ) {
+  tcc::data_structure::safe_static_vector<int, 4> vec{};
+  EXPECT_EQ( vec.push_back( 1, 2, 3, 4 ), 0 );
+  EXPECT_EQ( vec.push_back( 5 ), -1 );
+  EXPECT_EQ( vec.pop(), 0 );
+  EXPECT_EQ( vec.push_back( 5 ), 0 );
+  EXPECT_EQ( vec[3], 5 );
+}
+
+struct not_trivially_destructible {
+  static int m_calls;
+
+  ~not_trivially_destructible() {
+    ++not_trivially_destructible::m_calls;
+  }
+};
+
+int not_trivially_destructible::m_calls = 0;
+
+TEST( TestStaticVector, TestDestructorCalls ) {
+  tcc::data_structure::safe_static_vector<not_trivially_destructible, 2> outer;
+  not_trivially_destructible copy_me;
+  outer.push_back(copy_me, copy_me);
+  EXPECT_EQ( not_trivially_destructible::m_calls, 0 );
+  {
+    tcc::data_structure::safe_static_vector<not_trivially_destructible, 42> inner;
+    for( int i = 0; i < 38; ++i ) {
+      EXPECT_EQ( inner.push_back( copy_me ), 0 );
+    }
+    EXPECT_EQ( not_trivially_destructible::m_calls, 0 );
+  }
+  EXPECT_EQ( not_trivially_destructible::m_calls, 38 );
+}
+
+TEST( TestStaticVector, TestCopyConstructor ) {
+  tcc::data_structure::safe_static_vector<int, 8> original;
+  original.push_back( 1, 2, 3, 4 );
+  tcc::data_structure::safe_static_vector<int, 8> copy{ original };
+  for( int i = 0; i < 4; ++i ) {
+    EXPECT_EQ( original[ i ], i + 1 );
+    EXPECT_EQ( original[ i ], copy[ i ] );
+    copy[ i ]++;
+  }
+  for( int i = 0; i < 4; ++i ) {
+    //After modifying the copy, it shouldn't be equal to the original anymore...
+    EXPECT_EQ( original[ i ], i + 1 );
+    EXPECT_EQ( copy[ i ], i + 2 );
+    EXPECT_FALSE( original[ i ] == copy[ i ] );
+  }
+  //Assert we copied the correct number of elements. 4 remaining...
+  EXPECT_EQ( copy.push_back( 6, 7, 8, 9 ), 0 );
+  EXPECT_EQ( copy.push_back( -1 ), -1 );
+  //Changing the copy shouldn't change the original vector and vice-versa. Check the original vector pointers.
+  EXPECT_EQ( original.push_back( 5, 6, 7, 8 ), 0 );
+  EXPECT_EQ( original.push_back( -1 ), -1 );
+  //Compare elements. Should all be different.
+  for( int i = 0; i < 8; ++i ) {
+    EXPECT_EQ( original[ i ], i + 1 );
+    EXPECT_EQ( copy[ i ], i + 2 );
+  }
+
+}
+
+TEST( TestStaticVector, TestCopyAssignment ) {
+  tcc::data_structure::safe_static_vector<int, 8> original;
+  original.push_back( 1, 2, 3, 4 );
+  tcc::data_structure::safe_static_vector<int, 8> copy;
+  copy = original;
+  for( int i = 0; i < 4; ++i ) {
+    EXPECT_EQ( original[ i ], i + 1 );
+    EXPECT_EQ( original[ i ], copy[ i ] );
+    copy[ i ]++;
+  }
+  for( int i = 0; i < 4; ++i ) {
+    //After modifying the copy, it shouldn't be equal to the original anymore...
+    EXPECT_EQ( original[ i ], i + 1 );
+    EXPECT_EQ( copy[ i ], i + 2 );
+    EXPECT_FALSE( original[ i ] == copy[ i ] );
+  }
+  //Assert we copied the correct number of elements. 4 remaining...
+  EXPECT_EQ( copy.push_back( 6, 7, 8, 9 ), 0 );
+  EXPECT_EQ( copy.push_back( -1 ), -1 );
+  //Changing the copy shouldn't change the original vector and vice-versa. Check the original vector pointers.
+  EXPECT_EQ( original.push_back( 5, 6, 7, 8 ), 0 );
+  EXPECT_EQ( original.push_back( -1 ), -1 );
+  //Compare elements. Should all be different.
+  for( int i = 0; i < 8; ++i ) {
+    EXPECT_EQ( original[ i ], i + 1 );
+    EXPECT_EQ( copy[ i ], i + 2 );
+  }
+}
+
+TEST( TestStaticVector, TestCopySelfAssignment ) {
+  tcc::data_structure::safe_static_vector<int, 8> original;
+  original.push_back( 1, 2, 3, 4 );
+  EXPECT_EQ( original.size(), 4 );
+  for( int i = 0; i < 4; ++i ) {
+    EXPECT_EQ( original[ i ], i + 1 );
+  }
+  original = original;
+  EXPECT_EQ( original.size(), 4 );
+  for( int i = 0; i < 4; ++i ) {
+    EXPECT_EQ( original[ i ], i + 1 );
+  }
+}
+
+TEST( TestStaticVector, TestMoveConstructor ) {
+  //Should trigger a copy here. Just repeat copy test.
+  tcc::data_structure::safe_static_vector<int, 8> original;
+  original.push_back( 1, 2, 3, 4 );
+  tcc::data_structure::safe_static_vector<int, 8> copy{ std::move( original ) };
+  for( int i = 0; i < 4; ++i ) {
+    EXPECT_EQ( original[ i ], i + 1 );
+    EXPECT_EQ( original[ i ], copy[ i ] );
+    copy[ i ]++;
+  }
+  for( int i = 0; i < 4; ++i ) {
+    //After modifying the copy, it shouldn't be equal to the original anymore...
+    EXPECT_EQ( original[ i ], i + 1 );
+    EXPECT_EQ( copy[ i ], i + 2 );
+    EXPECT_FALSE( original[ i ] == copy[ i ] );
+  }
+  //Assert we copied the correct number of elements. 4 remaining...
+  EXPECT_EQ( copy.push_back( 6, 7, 8, 9 ), 0 );
+  EXPECT_EQ( copy.push_back( -1 ), -1 );
+  //Changing the copy shouldn't change the original vector and vice-versa. Check the original vector pointers.
+  EXPECT_EQ( original.push_back( 5, 6, 7, 8 ), 0 );
+  EXPECT_EQ( original.push_back( -1 ), -1 );
+  //Compare elements. Should all be different.
+  for( int i = 0; i < 8; ++i ) {
+    EXPECT_EQ( original[ i ], i + 1 );
+    EXPECT_EQ( copy[ i ], i + 2 );
+  }
+}
+
+TEST( TestStaticVector, TestMoveAssignment ) {
+  //Should trigger a copy here. Just repeat copy test.
+  tcc::data_structure::safe_static_vector<int, 8> original;
+  original.push_back( 1, 2, 3, 4 );
+  tcc::data_structure::safe_static_vector<int, 8> copy;
+  copy = std::move( original );
+  for( int i = 0; i < 4; ++i ) {
+    EXPECT_EQ( original[ i ], i + 1 );
+    EXPECT_EQ( original[ i ], copy[ i ] );
+    copy[ i ]++;
+  }
+  for( int i = 0; i < 4; ++i ) {
+    //After modifying the copy, it shouldn't be equal to the original anymore...
+    EXPECT_EQ( original[ i ], i + 1 );
+    EXPECT_EQ( copy[ i ], i + 2 );
+    EXPECT_FALSE( original[ i ] == copy[ i ] );
+  }
+  //Assert we copied the correct number of elements. 4 remaining...
+  EXPECT_EQ( copy.push_back( 6, 7, 8, 9 ), 0 );
+  EXPECT_EQ( copy.push_back( -1 ), -1 );
+  //Changing the copy shouldn't change the original vector and vice-versa. Check the original vector pointers.
+  EXPECT_EQ( original.push_back( 5, 6, 7, 8 ), 0 );
+  EXPECT_EQ( original.push_back( -1 ), -1 );
+  //Compare elements. Should all be different.
+  for( int i = 0; i < 8; ++i ) {
+    EXPECT_EQ( original[ i ], i + 1 );
+    EXPECT_EQ( copy[ i ], i + 2 );
+  }
 }
